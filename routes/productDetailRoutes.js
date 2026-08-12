888b925deba9f23ffdd0629b9d5cd2baee9a9a87
Purvesh Thite <purveshthite@gmail.com>
2026-08-13 02:50:33 +0530

    new commit wishlist

--- routes/productDetailRoutes.js
+++ routes/productDetailRoutes.js
@@ -38,17 +38,56 @@ router.get("/:id", (req, res) => {
 
             console.log("PRODUCT:", product);
             console.log("USER:", user);
+            const wishsql = "SELECT * FROM wishlist where product_id=?"
 
-            return res.render("listings/productDetailsPage.ejs", {
+            db.query(wishsql,[id],(err,wishResult)=>{
+
+                const wishlist = wishResult[0] || null;;
+                 return res.render("listings/productDetailsPage.ejs", {
                 activePage: "",
                 product: product,
-                user: user
+                user: user,
+                wishlist: wishlist
             });
+            })
+
+           
 
         });
 
     });
 
 });
+router.post("/wishlist", (req, res) => {
 
+    const { product_id } = req.body;
+    const user_id = req.session.user_id;
+    const created_at = new Date();
+
+    if (!user_id) {
+        return res.redirect("/login");
+    }
+
+    const wishlist_id = crypto.randomUUID();
+
+    const sql = `
+        INSERT INTO wishlist
+        (wishlist_id, user_id, product_id, created_at)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    db.query(
+        sql,
+        [wishlist_id, user_id, product_id, created_at],
+        (err, result) => {
+
+            if (err) {
+                console.log(err);
+                return res.status(500).send(err.sqlMessage);
+            }
+
+            res.redirect(`/productDetails/${product_id}`);
+        }
+    );
+});
 module.exports = router;
--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -3,8 +3,28 @@ const router = express.Router();
 const db = require("../db/connection");
 
 // wishlist route
-router.get("/",(req,res)=>{
-    res.render("listings/wishlist.ejs",{activePage: "wishlist"});
+router.get("/", (req, res) => {
+
+    const sql = `
+        SELECT products.*
+        FROM wishlist
+        JOIN products
+        ON wishlist.product_id = products.product_id
+        WHERE wishlist.user_id = ?
+    `;
+
+    db.query(sql, [req.session.user_id], (err, products) => {
+
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err.sqlMessage);
+        }
+
+        res.render("listings/wishlist.ejs", {
+            activePage: "wishlist",
+            products: products
+        });
+    });
 });
 
 module.exports = router;
