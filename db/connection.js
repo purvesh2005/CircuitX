c747821627ce8516bcd90b48075d5f17c898a3ce
Purvesh Thite <purveshthite@gmail.com>
2026-08-13 15:55:26 +0530

    .env added

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,26 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Purvesh123*',
-  database: 'circuitx',
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-db.connect();
-
 db.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.log("Database connection failed:", err);
+        return;
+    }
+
+    console.log("MySQL connected!");
 
     db.query("CREATE DATABASE IF NOT EXISTS circuitx", (err) => {
-        if (err) throw err;
+        if (err) {
+            console.log("Database creation error:", err);
+            return;
+        }
 
         console.log("Database ready!");
     });
