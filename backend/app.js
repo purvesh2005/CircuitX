e2cdc7a30932f1dfc48821a839df694f7d2b8af1
Purvesh Thite <purveshthite@gmail.com>
2026-08-15 19:50:54 +0530

    structure changed and frontend and backend folder created

--- backend/app.js
+++ backend/app.js
@@ -24,13 +24,13 @@ const profileRoutes = require("./routes/profileRoutes");
 
 // EJS
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../frontend/views"));
 app.engine("ejs", ejsMate);
 
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../frontend/public")));
 
 
 // SESSION
